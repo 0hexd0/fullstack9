a79d6d3ed1fb791a5be040ae49aa8c03a95997f8
HeXudong <1514133606@qq.com>
2023-01-29 17:03:13 +0800

    feat:add User and Token

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const {
+  ApolloServer,
+  UserInputError,
+  AuthenticationError,
+  gql,
+} = require('apollo-server')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -49,6 +57,7 @@ const typeDefs = gql`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -86,9 +95,15 @@ const resolvers = {
       })
       return authors
     },
+    me: async (root, args, context) => {
+      return context.currentUser
+    },
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError('invalid user')
+      }
       const authorName = args.author
       let author = await Author.findOne({ name: authorName })
       if (!author) {
@@ -109,7 +124,10 @@ const resolvers = {
       savedBook.author = author
       return savedBook
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError('invalid user')
+      }
       const author = await Author.findOne({ name: args.name })
       if (!author) {
         return null
@@ -125,12 +143,46 @@ const resolvers = {
       }
       return updatedAuthor
     },
+    createUser: async (root, args) => {
+      const user = new User({
+        username: args.username,
+        favouriteGenre: args.favouriteGenre,
+      })
+
+      return user.save().catch((error) => {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError('wrong credentials')
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
   },
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
 })
 
 server.listen().then(({ url }) => {
